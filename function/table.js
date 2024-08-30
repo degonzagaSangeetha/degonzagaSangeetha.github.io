9628e71b1d479368e39db0cf890df7946bb9a840
Naren-2001-k <k.naren2001@gmail.com>
2024-08-30 11:11:22 +0530

    afa

---
+++ function/table.js
@@ -0,0 +1,18 @@
+<script>
+    function calculate(a,b)
+    {
+        let result=[]
+        for(let i=1; i<=b ; i++){
+      
+     result= a*i
+
+    }
+    return result;}
+ 
+
+let a=parseInt(prompt("which table no."))
+let b=parseInt(prompt("upto what no."))
+let c=calculate(a,b)
+for(let table:result)
+
+</script>
